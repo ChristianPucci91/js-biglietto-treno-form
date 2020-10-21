d153e60743a3bfcd16b207e8e638d2f2cbacfebd
ChristianPucci91 <pucci.christian91@gmail.com>
2020-10-21 15:55:19 +0200

    testing input value and adding first variables

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Dare display none a il biglietto
+
+document.getElementById("biglietto").style.display = "none";
+
+// Variabili da stabilire
+
+var input1 = document.getElementById('nome');
+console.log(input1.value);
+
+var input2 = document.getElementById('km');
+console.log(input2.value);
+
+var input3 = document.getElementById('eta');
+console.log(input3.value);
+
+// Variabili di output
+
+nome_pax = document.getElementById('passeggero')
+
+
+
+// prezzo biglietto ( 0.21€ per km * km inseriti dall'utente)
+
+var prezzoBiglietto = ( km * 0.21 );
+
+// prezzo biglietto scontato 20% ( prezzo intero - prezzo percentuale = prezzo scontato)
+
+var prezzoScontato20 = ( prezzoBiglietto - ( prezzoBiglietto * 0.20 ));
+
+// prezzo biglietto scontato 40% ( prezzo intero - prezzo percentuale = prezzo scontato)
+
+var prezzoScontato40 = ( prezzoBiglietto - ( prezzoBiglietto * 0.40 ));
+
+
+// tasti genera e annulla
+
+// OUTPUT
+// Nome passeggero
+
+// Tipologia Sconto
+
+// Numero carrozza
+
+// Codice CP
+
+// Costo biglietto
