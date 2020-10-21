c2e1ee575f3b2bd6d399e82f53bccc3d3704f092
ChristianPucci91 <pucci.christian91@gmail.com>
2020-10-21 19:10:38 +0200

    Fixed all errors, exercise done.

--- js/script.js
+++ js/script.js
@@ -1,71 +1,90 @@
-// Dare display none a il biglietto
+// mettiamo in display none la parte del biglietto
 
 document.getElementById("biglietto").style.display = "none";
 
-//variabile button Genera & annulla
+// Diamo delle variabili ai due bottoni
 
-var button1 = document.getElementById('button_genera');
-var button2 = document.getElementById('button_annulla');
+var button1 = document.getElementById("button_genera");
+var button2 = document.getElementById("button_annulla");
 
-// Otteniamo il valore dei campi e gli diamo una variabile
+// Valori di output
 
-var input1 = document.getElementById('nome').value;
-// console.log(input1.value);
-var input2 = document.getElementById('km').value;
-// console.log(input2.value);
-var input3 = document.getElementById('eta').value;
-// console.log(input3.value);
-var eta = document.getElementById('eta').value;
-// console.log(eta);
+var sconto = document.getElementById('sconto')
 
+// diamo le funzionalità del bottone genera
 
-// Variabili di calcolo prezzo intero / sconto 20% / sconto 40%
+  button1.addEventListener('click',
+    function() {
 
-var prezzoBiglietto = ( input2 * 0.21 );
-// console.log(costoBiglietto);
-var prezzoScontato20 = ( prezzoBiglietto - ( prezzoBiglietto * 0.20 ));
-// console.log(prezzoScontato20);
-var prezzoScontato40 = ( prezzoBiglietto - ( prezzoBiglietto * 0.40 ));
-// console.log(prezzoScontato40);
+      // Ottieni valori input utente
+      var nome = document.getElementById('nome').value;
+      //console.log(nome);
+      var km = document.getElementById('km').value;
+      //console.log(km);
+      var eta = document.getElementById('eta').value;
+      console.log(eta);
 
-// Variabili di output
+      // mettiamo in display block la parte del BIGLIETTO
+      document.getElementById("biglietto").style.display = "block";
 
-var nome_pax = document.getElementById('passeggero');
-var offerta = document.getElementById('sconto');
-var costo_biglietto = document.getElementById('costo');
-var carrozza = document.getElementById('carrozza');
-var codice = document.getElementById('codice');
+      // Calcolo biglietto
+       var costoBiglietto = 0.21 * km;
+       document.getElementById('costo').innerHTML = costoBiglietto;
 
-// Function genera
+       // output tariffa standard
+       sconto = "Tariffa <br> standard";
 
-    button1.addEventListener('click',
-     function() {
+       // calcolo sconto a seconda della categoria pax
 
-           // output nome utente
-           nome_pax.innerHTML = input1;
-           // output tipologia sconto
-           offerta.innerHTML = input3;
-           // output carrozza da 1 a 10
-           carrozza.innerHTML = Math.floor(Math.random() * 10) + 1;
-           // output codice cp da 1 a 100000
-           codice.innerHTML = Math.floor(Math.random() * 100000 ) + 1 ;
-           // display block del box biglietto
-           document.getElementById("biglietto").style.display = "block";
+         if ( eta == "minorenne" ) {
 
-              // condizioni if & else
+           // sconto 20%
+           costoBiglietto -= costoBiglietto * 0.2;
+           sconto = "Sconto <br> Minorenne";
+         } else if ( eta == "over65" ) {
 
+           // sconto 40%
+           costoBiglietto -= costoBiglietto * 0.4;
+           sconto = "Sconto <br> Over 65";
+         }
 
-    } );
+         // decimali
+        costoBiglietto = costoBiglietto.toFixed(2) + '€';
 
-// Function annulla
+        // Numero random per la carrozza da 1 a 10
+        var carrozza = Math.floor( Math.random() * 10) + 1;
 
+        // Numero random per codice cp da 1 a 100000
+        var codice = Math.floor( Math.random() * 100000  ) + 1;
+
+        // Inseriamo valori nella pagina
+        document.getElementById('passeggero').innerHTML = nome;
+        document.getElementById('sconto').innerHTML = sconto;
+        document.getElementById('carrozza').innerHTML = carrozza;
+        document.getElementById('codice').innerHTML = codice;
+        document.getElementById('costo').innerHTML = costoBiglietto;
+
+  }
+  );
+
+      // Resettare il biglietto
     button2.addEventListener('click',
-     function() {
-           // dare un valore nullo al click annulla
-           input1.value = ('');
-           input2.value = (1);
-           input3.value = ('');
-           // display none del box biglietto dopo il click annulla
-           document.getElementById("biglietto").style.display = "none";
+      function() {
 
-    } );
+        // Reset campi da compilare
+        document.getElementById('nome').value = '';
+        document.getElementById('km').value = 1;
+        document.getElementById('eta').value = ('');
+
+        // Reset biglietto
+        document.getElementById('sconto').innerHTML = '';
+        document.getElementById('carrozza').innerHTML = '';
+        document.getElementById('codice').innerHTML = '';
+        document.getElementById('costo').innerHTML = '';
+
+        // rimettiamo in display none la parte del biglietto
+
+        document.getElementById("biglietto").style.display = "none";
+
+      }
+    );
